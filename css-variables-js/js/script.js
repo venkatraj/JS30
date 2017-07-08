be0a6a5bc08f9d420621ae1b9458f679c68dd21e
Venkat <venkatraj.n@gmail.com>
2017-07-08 11:09:52 +0530

    removed files

--- css-variables-js/js/script.js
+++
@@ -1,9 +0,0 @@
-const inputs = document.querySelectorAll('.controls input');
-
-function handle() {
-  const suffix = this.dataset.sizing || '';
-  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-}
-
-inputs.forEach(input => input.addEventListener('change',handle));
-inputs.forEach(input => input.addEventListener('mousemove',handle));
